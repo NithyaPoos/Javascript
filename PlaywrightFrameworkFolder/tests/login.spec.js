e192d1f2e3b2da87f4ccf65a72ce8bcfaea6bd81
nithyapoos <twinnature99@gmail.com>
2025-09-13 12:38:23 -0700

    added invalid credentials

--- PlaywrightFrameworkFolder/tests/login.spec.js
+++ PlaywrightFrameworkFolder/tests/login.spec.js
@@ -4,17 +4,20 @@ import testdata from '../test-data/testdata.json';
 
 
 
-test('login test',async ({page})=>{
+test('invlaidlogin test',async ({page})=>{
 
     //login page object
     const lp=new LoginPage(page);
     await lp.launchApp();
     await lp.enterUsername(testdata.loginPage.validUsername);
-    await lp.enterPassword(testdata.loginPage.validPassword);
+    await lp.enterPassword(testdata.loginPage.invalidPassword);
     await lp.clickLogin();
     const expectedTitle="Home Page ~ Salesforce - Developer Edition";
     const actualTitle=await lp.verifyTitle();
-    expect(actualTitle).toBe(expectedTitle);
+    // expect(actualTitle).toBe(expectedTitle);
+    expect(actualTitle).not.toBe(expectedTitle);
+    const actualErrorMessage=await lp.getErrorMessage();
+    expect(actualErrorMessage).toBe(testdata.loginPage.errorMessage); 
   //expect().toBe("Home Page ~ Salesforce - Developer Edition");
 })
 
